ce0e40b1f84b57f4cf59f0907ea69bdbc4445640
holmes2 <priyankkapadia89@gmail.com>
2015-09-28 22:07:35 -0400

    Initial Commit for Windows Service

---
+++ Bnk.Log.Monitor/Areas/HeartBeat/HeartBeatReceiver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bnk.Log.Monitor.Areas.HeartBeat
+{
+    /// <summary>
+    /// HeartBeat Receiver which receives the message from the Payment Reader
+    /// </summary>
+    class HeartBeatReceiver
+    {
+        public const int CRITICAL_TIMEOUT = 60;
+        public const int MAJOR_TIMEOUT = 40;
+        public const int WARNING_TIMEOUT = 20;
+
+        #region Properties 
+            
+
+        #endregion
+
+    }
+}
